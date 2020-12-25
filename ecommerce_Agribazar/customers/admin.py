c6c4f02ba27caaff5cbde90a41775858f98d842a
mahfuz00741 <mahfuzur35-2207@diu.edu.bd>
2020-12-25 12:44:50 +0600

    admin site customer liast display updated

--- ecommerce_Agribazar/customers/admin.py
+++ ecommerce_Agribazar/customers/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from customers.models import Customer
 
 # Register your models here.
-admin.site.register(Customer)
+class Admin_display(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'mobile_number']
+
+admin.site.register(Customer, Admin_display)
 
 
