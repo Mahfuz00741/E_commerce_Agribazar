e352d9d3f9b353e30c732026f5000dd16f7b0b27
mahfuz00741 <mahfuzur35-2207@diu.edu.bd>
2020-12-25 19:30:58 +0600

    contact us template added

--- ecommerce_Agribazar/pages/views.py
+++ ecommerce_Agribazar/pages/views.py
@@ -3,9 +3,14 @@ from django.views import View
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'pages/index.html')
+class Index(View):
+    def get(self, request):
+        return render(request, 'pages/index.html')
 
 class About(View):
     def get(self, request):
         return render(request, 'pages/about.html')
+
+class contact(View):
+    def get(self, request):
+        return render(request, 'pages/contact.html')
